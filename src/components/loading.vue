2634c9149c2c8d5894fbf0e8c72e95fb7e5e0fa0
Luccas Maia <luccasmaia@poli.ufrj.br>
2020-03-13 18:16:01 -0300

    first commit

---
+++ src/components/loading.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-layout row justify-center>
+    <v-dialog
+      v-model="loading"
+      persistent
+      fullscreen
+      content-class="loading-dialog"
+    >
+      <v-container fill-height>
+        <v-layout row justify-center align-center>
+          <v-progress-circular
+            indeterminate
+            :size="70"
+            :width="7"
+            color="orange"
+          >
+            <h1 class="color-loading">Aguarde...<br /><br /><br /><br /></h1>
+          </v-progress-circular>
+        </v-layout>
+      </v-container>
+    </v-dialog>
+  </v-layout>
+</template>
+<script>
+export default {
+  name: "loading",
+  props: { loading: Boolean }
+};
+</script>
+<style>
+.color-loading {
+  color: #ffffff;
+  stop-opacity: inherit;
+}
+.loading-dialog {
+  background-color: #000000;
+  opacity: 0.8;
+}
+td.img-table {
+  cursor: pointer;
+  user-select: none;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    vuetify,
+    render: h => h(App)
+}).$mount("#app");
